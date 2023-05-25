64430aefd44e796868517c4ddef3e87bfe6947d4
aberrantian <aberrantian@gmail.com>
2023-05-26 00:03:04 +0100

    fix swap

--- chapter-2/swap.c
+++ chapter-2/swap.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
-void swap(int a, int b) {
-  int t = a;
-  a = b;
-  b = t;
-
-  printf("swap: a = %d, b = %d\n", a, b);
+void swap(int *pa, int *pb) {
+  int t = *pa;
+  *pa = *pb;
+  *pb = t;
+  return;
 }
 
 int main(void) {
   int a = 1;
   int b = 2;
 
-  swap(a, b);
-  printf("main: a = %d, b = %d\n", a, b);
+  swap(&a, &b);
+  printf("a = %d, b = %d\n", a, b);
 
   return 0;
 }
