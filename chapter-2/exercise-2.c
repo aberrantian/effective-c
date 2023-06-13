9181fec185795c5d81d08702d7761d04d9ce7b7a
aberrantian <aberrantian@gmail.com>
2023-06-13 02:47:00 +0100

    reformat exercise 2

--- chapter-2/exercise-2.c
+++ chapter-2/exercise-2.c
@@ -1,10 +1,6 @@
 // Declare an array of three pointers to functions and invoke the appropriate function based on an index value passed in as an argument
 #include <stdio.h>
 
-// array of 3 elements of pointers to integers
-int (*array[3])(void);
-
-// create functions
 int f1(void) {
     return 1;
 }
@@ -17,13 +13,14 @@ int f3(void) {
     return 3;
 }
 
-int main(void) {
-    array[0] = &f1;
-    array[1] = &f2;
-    array[2] = &f3;
+int (*array[3])(void) = {
+    &f1,
+    &f2,
+    &f3
+};
 
+int main(void) {
     int output = (*array[0])();
     printf("%d\n", output);
-    
     return 0;
 }
